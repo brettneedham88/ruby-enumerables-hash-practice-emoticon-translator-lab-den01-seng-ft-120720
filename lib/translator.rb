bafc089b7a90965a10ffc872872da9a6ecc56219
Brett Needham <brettneedham88@gmail.com>
2020-11-25 18:09:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ def get_japanese_emoticon(file_path, emoticon)
  emoticons = YAML.load_file(file_path)
  emoticons.each do |key, value|
    if emoticons[key][0] == emoticon
-     puts key[1]
+     puts emoticons[key][1]
    else 
      puts "Sorry, that emoticon was not found"
    #binding.pry
